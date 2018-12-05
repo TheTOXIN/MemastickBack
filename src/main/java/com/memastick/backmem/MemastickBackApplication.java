987c7630e0b26f62159c46de542eb778acb217f1
toxin <yuri.belousov.1998@gmail.com>
2018-12-05 14:47:49 +0300

    init commit

---
+++ src/main/java/com/memastick/backmem/MemastickBackApplication.java
@@ -0,0 +1,12 @@
+package com.memastick.backmem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemastickBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemastickBackApplication.class, args);
+	}
+}
