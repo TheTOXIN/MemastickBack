18953473f46b9955ff98106e84317b1ff677497b
toxin <yuri.belousov.1998@gmail.com>
2019-01-31 19:21:37 +0300

    f max len nick

--- src/main/java/com/memastick/backmem/main/util/ValidationUtil.java
+++ src/main/java/com/memastick/backmem/main/util/ValidationUtil.java
@@ -3,7 +3,7 @@ package com.memastick.backmem.main.util;
 public class ValidationUtil {
 
     private static int MIN_LEN_NCK = 3;
-    private static int MAX_LEN_NCK = 15;
+    private static int MAX_LEN_NCK = 16;
 
     private static final String EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
     private static final String LOGIN_PATTERN = "((?=.*[A-Za-z0-9])[A-Za-z][A-Za-z0-9]{4,20})";
