7c3ce6c511fec25b3f62e51c259f0855ba4f68a7
toxin <yuri.belousov.1998@gmail.com>
2019-02-07 15:02:08 +0300

    f version

--- src/main/java/com/memastick/backmem/main/controller/HelloController.java
+++ src/main/java/com/memastick/backmem/main/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.memastick.backmem.main.controller;
 
+import com.memastick.backmem.main.constant.GlobalConstant;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello() {
-        return "Hello, i'm MEMASTICK server!!!";
+        return "Hello, i'm MEMASTICK server!!! ver: " + GlobalConstant.VERSION;
     }
 
 }
