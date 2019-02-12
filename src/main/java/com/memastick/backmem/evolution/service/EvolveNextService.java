b291d8f5b424bbeeacb4bed014fd81000bcd3176
toxin <yuri.belousov.1998@gmail.com>
2019-02-12 14:48:38 +0300

    + logs evolve

--- src/main/java/com/memastick/backmem/evolution/service/EvolveNextService.java
+++ src/main/java/com/memastick/backmem/evolution/service/EvolveNextService.java
@@ -4,6 +4,8 @@ import com.memastick.backmem.evolution.constant.EvolveStep;
 import com.memastick.backmem.evolution.entity.EvolveMeme;
 import com.memastick.backmem.evolution.handler.EvolveHandler;
 import com.memastick.backmem.evolution.repository.EvolveMemeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class EvolveNextService {
+
+    private static final Logger log = LoggerFactory.getLogger(EvolveNextService.class);
 
     private final EvolveHandler evolveHandler;
     private final EvolveMemeRepository evolveMemeRepository;
@@ -31,6 +35,8 @@ public class EvolveNextService {
 
     @Scheduled(cron = "0 0 0 * * *", zone = "UTC")
     public void next() {
+        log.info("START EVOLVE - {}", evolveMemeService.evolveDay());
+
         Arrays.stream(EvolveStep.values()).forEach(step -> {
             List<EvolveMeme> evolveMemes = evolveMemeRepository.findByStep(step);
 
@@ -39,5 +45,7 @@ public class EvolveNextService {
 
             evolveMemeRepository.saveAll(evolveMemes);
         });
+
+        log.info("END EVOLVE - {}", evolveMemeService.evolveDay());
     }
 }
