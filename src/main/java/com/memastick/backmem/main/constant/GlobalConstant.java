0bb407b3a8d7c3cb01817455dc8fa19a43c321ea
TheTOXIN <thetoksin@gmail.com>
2019-03-17 19:39:00 +0300

    increaser version

--- src/main/java/com/memastick/backmem/main/constant/GlobalConstant.java
+++ src/main/java/com/memastick/backmem/main/constant/GlobalConstant.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class GlobalConstant {
 
-    public static final String VERSION = "0.2.1 alpha";
+    public static final String VERSION = "0.2.2 alpha";
 
     public static final LocalDate START_EVOLVE = LocalDate.of(2019, 1, 1);
 
