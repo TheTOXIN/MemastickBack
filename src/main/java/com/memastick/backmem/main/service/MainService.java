e5324d09334e049cfa567e9d6673f7d486f53eaa
toxin <yuri.belousov.1998@gmail.com>
2019-05-06 13:48:46 +0300

    + count evolve memes to home api

--- src/main/java/com/memastick/backmem/main/service/MainService.java
+++ src/main/java/com/memastick/backmem/main/service/MainService.java
@@ -2,8 +2,9 @@ package com.memastick.backmem.main.service;
 
 import com.memastick.backmem.evolution.service.EvolveMemeService;
 import com.memastick.backmem.main.api.HomeAPI;
+import com.memastick.backmem.memes.constant.MemeType;
+import com.memastick.backmem.memes.repository.MemeRepository;
 import com.memastick.backmem.security.service.SecurityService;
-import com.memastick.backmem.tokens.service.TokenAllowanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +13,24 @@ public class MainService {
 
     private final SecurityService securityService;
     private final EvolveMemeService evolveMemeService;
-    private final TokenAllowanceService tokenAllowanceService;
+    private final MemeRepository memeRepository;
 
     @Autowired
     public MainService(
-            SecurityService securityService,
-            EvolveMemeService evolveMemeService,
-            TokenAllowanceService tokenAllowanceService
+        SecurityService securityService,
+        EvolveMemeService evolveMemeService,
+        MemeRepository memeRepository
     ) {
         this.securityService = securityService;
         this.evolveMemeService = evolveMemeService;
-        this.tokenAllowanceService = tokenAllowanceService;
+        this.memeRepository = memeRepository;
     }
 
     public HomeAPI home() {
         return new HomeAPI(
             securityService.getCurrentMemetick().getNick(),
             evolveMemeService.evolveDay(),
-            tokenAllowanceService.have()
+            memeRepository.countByType(MemeType.EVOLVE).orElse(0L)
         );
     }
 }
