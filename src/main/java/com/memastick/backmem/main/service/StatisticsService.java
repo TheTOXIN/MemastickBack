6e8e7c61b5d574954e5f436fe8c5f3e244a1c449
toxin <yuri.belousov.1998@gmail.com>
2019-06-18 13:27:49 +0300

    + cache global stats

--- src/main/java/com/memastick/backmem/main/service/StatisticsService.java
+++ src/main/java/com/memastick/backmem/main/service/StatisticsService.java
@@ -1,5 +1,6 @@
 package com.memastick.backmem.main.service;
 
+import com.memastick.backmem.evolution.service.EvolveMemeService;
 import com.memastick.backmem.main.api.StatisticsAPI;
 import com.memastick.backmem.memes.constant.MemeType;
 import com.memastick.backmem.memes.repository.MemeRepository;
@@ -7,21 +8,28 @@ import com.memastick.backmem.memetick.repository.MemetickRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class StatisticsService {
 
+    private Map<Long, StatisticsAPI> globalCache = new HashMap<>();
+
     private final MemetickRepository memetickRepository;
     private final MemeRepository memeRepository;
+    private final EvolveMemeService evolveMemeService;
 
     @Autowired
     public StatisticsService(
         MemetickRepository memetickRepository,
-        MemeRepository memeRepository
+        MemeRepository memeRepository,
+        EvolveMemeService evolveMemeService
     ) {
         this.memetickRepository = memetickRepository;
         this.memeRepository = memeRepository;
+        this.evolveMemeService = evolveMemeService;
     }
 
     public StatisticsAPI byMemetick(UUID memetickId) {
@@ -33,10 +41,16 @@ public class StatisticsService {
     }
 
     public StatisticsAPI global() {
-        return new StatisticsAPI(
+        long evolveDay = evolveMemeService.evolveDay();
+
+        StatisticsAPI stats = globalCache.getOrDefault(evolveDay, new StatisticsAPI(
             memetickRepository.sumDna().orElse(0L),
             memeRepository.countByType(MemeType.INDIVID).orElse(0L),
             memeRepository.sumChromosome().orElse(0L)
-        );
+        ));
+
+        globalCache.put(evolveDay, stats);
+
+        return stats;
     }
 }
