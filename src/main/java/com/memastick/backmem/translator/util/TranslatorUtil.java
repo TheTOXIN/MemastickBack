065b128de14ce40717dd4f3acb607f35df014d46
toxin <yuri.belousov.1998@gmail.com>
2019-07-19 13:47:18 +0300

    + text to translate

--- src/main/java/com/memastick/backmem/translator/util/TranslatorUtil.java
+++ src/main/java/com/memastick/backmem/translator/util/TranslatorUtil.java
@@ -6,12 +6,17 @@ import com.memastick.backmem.memes.entity.Meme;
 public class TranslatorUtil {
 
     public static String prepareText(Meme meme) {
-        return new StringBuilder()
-            .append("МЕМ ДНЯ ❗️" + "\n" )
+        StringBuilder builder = new StringBuilder();
+
+        if (meme.getText() != null) builder.append("\"" + meme.getText() + "\"");
+
+        builder
+            .append("МЕМ ДНЯ ❗️" + "\n")
             .append("\uD83C\uDF0E Эволюция №" + meme.getEvolution() + "\n")
             .append("\uD83D\uDE0E Меметик - " + meme.getMemetick().getNick() + "\n")
             .append("☘️ Хромосом: " + meme.getChromosomes() + "\n")
-            .append(GlobalConstant.URL + "/memes/share/" + meme.getId())
-            .toString();
+            .append(GlobalConstant.URL + "/memes/share/" + meme.getId());
+
+        return builder.toString();
     }
 }
