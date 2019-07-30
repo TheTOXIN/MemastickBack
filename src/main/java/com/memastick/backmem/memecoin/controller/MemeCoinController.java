1dd3be33710d2b2bdd3a5dbd57edc7609f3c04e5
toxin <yuri.belousov.1998@gmail.com>
2019-07-30 14:00:00 +0300

    fix prod #2

--- src/main/java/com/memastick/backmem/memecoin/controller/MemeCoinController.java
+++ src/main/java/com/memastick/backmem/memecoin/controller/MemeCoinController.java
@@ -3,11 +3,14 @@ package com.memastick.backmem.memecoin.controller;
 import com.memastick.backmem.memecoin.api.MemeCoinAPI;
 import com.memastick.backmem.memecoin.repository.MemeCoinRepository;
 import com.memastick.backmem.memecoin.service.MemeCoinService;
+import com.memastick.backmem.memetick.service.MemetickService;
 import com.memastick.backmem.security.component.OauthData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("meme-coins")
@@ -15,17 +18,20 @@ public class MemeCoinController {
 
     private final MemeCoinRepository coinRepository;
     private final MemeCoinService memeCoinService;
+    private final MemetickService memetickService;
     private final OauthData oauthData;
 
     @Autowired
     public MemeCoinController(
         MemeCoinRepository coinRepository,
         MemeCoinService memeCoinService,
-        OauthData oauthData
+        OauthData oauthData,
+        MemetickService memetickService
     ) {
         this.coinRepository = coinRepository;
         this.memeCoinService = memeCoinService;
         this.oauthData = oauthData;
+        this.memetickService = memetickService;
     }
 
     @GetMapping("history")
@@ -37,9 +43,12 @@ public class MemeCoinController {
     }
 
     @PutMapping("transaction/{value}")
-    public void transaction(@PathVariable("value") long value) {
+    public void transaction(
+        @PathVariable("value") long value,
+        @RequestBody UUID memetickId
+    ) {
         memeCoinService.transaction(
-            oauthData.getCurrentMemetick(),
+            memetickService.findById(memetickId),
             value
         );
     }
