901753f845f1392307a481c3ea65481bafa7d4e3
TheTOXIN <thetoksin@gmail.com>
2019-09-07 19:08:14 +0300

    f migration

--- src/main/resources/migration/0.5/scripts/01_0.5.sql
+++ src/main/resources/migration/0.5/scripts/01_0.5.sql
@@ -1,2 +1,2 @@
-ALTER TABLE token_wallets ADD COLUMN IF NOT EXISTS memetick_id uuid constraint fk2iwq6k17uujix2kv6xwwk4h5t references memeticks;
+ALTER TABLE token_wallets ADD COLUMN memetick_id uuid constraint fk2iwq6k17uujix2kv6xwwk4h5t references memeticks;
 UPDATE token_wallets AS tw SET memetick_id = mi.memetick_id FROM memetick_inventories AS mi WHERE mi.token_wallet_id = tw.id;
--- src/main/resources/migration/0.5/scripts/05_0.5.sql
+++ src/main/resources/migration/0.5/scripts/05_0.5.sql
@@ -1,3 +1,3 @@
-ALTER TABLE users ADD COLUMN IF NOT EXISTS ban boolean;
+ALTER TABLE users ADD COLUMN ban boolean;
 UPDATE users SET ban = false ;
 ALTER TABLE users ALTER COLUMN ban SET NOT NULL;
