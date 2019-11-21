0dc257191b0ebbaf0c915dbab001974e831dd07d
toxin <yuri.belousov.1998@gmail.com>
2019-11-21 19:38:42 +0300

    f pattern validation

--- src/main/java/com/memastick/backmem/security/controller/RegistrationController.java
+++ src/main/java/com/memastick/backmem/security/controller/RegistrationController.java
@@ -3,7 +3,7 @@ package com.memastick.backmem.security.controller;
 import com.memastick.backmem.security.api.RegistrationAPI;
 import com.memastick.backmem.security.constant.SecurityStatus;
 import com.memastick.backmem.security.service.RegistrationService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,16 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 public class RegistrationController {
 
     private final RegistrationService registrationService;
-
-    @Autowired
-    public RegistrationController(
-        RegistrationService registrationService
-    ) {
-        this.registrationService = registrationService;
-    }
 
     @PostMapping("registration")
     public ResponseEntity<SecurityStatus> registration(@RequestBody RegistrationAPI request) {
