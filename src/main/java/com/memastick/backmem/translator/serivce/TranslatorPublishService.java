f61af63cc3eb60e3af08b12fb4b153744a2e3d61
toxin <yuri.belousov.1998@gmail.com>
2020-01-29 10:13:34 +0300

    wtf

--- src/main/java/com/memastick/backmem/translator/serivce/TranslatorPublishService.java
+++ src/main/java/com/memastick/backmem/translator/serivce/TranslatorPublishService.java
@@ -31,7 +31,7 @@ public class TranslatorPublishService {
     private final NotifyService notifyService;
 
     @Transactional
-    @Scheduled(cron = "0 0 5 * * *", zone = "UTC")
+    @Scheduled(cron = "0 0 3 * * *", zone = "UTC")
     public void publish() {
         log.info("START TRANSLATE PUBLISH");
 
