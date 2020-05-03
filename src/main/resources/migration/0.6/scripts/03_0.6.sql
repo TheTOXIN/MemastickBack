128383710aec3afc186015be7a7b3236ef4ac7e0
TheTOXIN <thetoksin@gmail.com>
2020-05-03 20:28:13 +0300

    PIZDAAAAAAAAAAAA

--- src/main/resources/migration/0.6/scripts/03_0.6.sql
+++ src/main/resources/migration/0.6/scripts/03_0.6.sql
@@ -1,3 +1,3 @@
 ALTER TABLE memetick_inventories ADD COLUMN cell_combo integer;
-UPDATE memetick_inventories SET cell_combo = 0 WHERE true;
+UPDATE memetick_inventories SET cell_combo = 1 WHERE true;
 ALTER TABLE memetick_inventories ALTER COLUMN cell_combo SET NOT NULL;
