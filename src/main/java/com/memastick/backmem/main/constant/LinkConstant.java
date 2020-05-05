aa94331cac16e489f245be4af9aab544485e5838
TheTOXIN <thetoksin@gmail.com>
2020-05-05 13:53:06 +0300

    improve notify tokens

--- src/main/java/com/memastick/backmem/main/constant/LinkConstant.java
+++ src/main/java/com/memastick/backmem/main/constant/LinkConstant.java
@@ -4,28 +4,18 @@ package com.memastick.backmem.main.constant;
 import static com.memastick.backmem.main.constant.GlobalConstant.URL;
 
 public class LinkConstant {
-    
+
     public static final String LINK_DNA = URL + "/modal?type=" + ModalType.DNA;
-
     public static final String LINK_MEMECOINS = URL + "/modal?type=" + ModalType.MEMECOIN;
-
     public static final String LINK_ALLOWANCE = URL + "/modal?type=" + ModalType.ALLOWANCE;
-
     public static final String LINK_BATTLE = URL + "/battle/view";
-
     public static final String LINK_CREATING = URL + "/memes/create";
-
     public static final String LINK_MEME = URL + "/memes/share";
-
+    public static final String LINK_RESEARCH = URL + "/memes/research";
     public static final String LINK_MEMOTYPES = URL + "/memotype/collection";
-
     public static final String LINK_MEMETICK = URL + "/home/memetick";
-
     public static final String LINK_ICON = URL + "/assets/images/title-logo.png";
-
     public static final String LINK_MEME_BAN = URL + "/assets/images/other/meme_ban.png";
-
     public static final String LINK_REG_INVITE = URL + "/pages/sign-up?invite=";
-
     public static final String NETRAL_AVATAR = URL + "/assets/images/other/netral_avatar.png";
 }
