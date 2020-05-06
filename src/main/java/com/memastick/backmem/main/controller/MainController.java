2445b2aad97b0002e9bf5b7cbf3f5cb35039630c
TheTOXIN <thetoksin@gmail.com>
2020-05-06 18:02:35 +0300

    move vers

--- src/main/java/com/memastick/backmem/main/controller/MainController.java
+++ src/main/java/com/memastick/backmem/main/controller/MainController.java
@@ -4,19 +4,16 @@ import com.memastick.backmem.main.api.HomeAPI;
 import com.memastick.backmem.main.api.NotifyCountAPI;
 import com.memastick.backmem.main.constant.GlobalConstant;
 import com.memastick.backmem.main.service.MainService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MainController {
 
     private final MainService mainService;
-
-    @Autowired
-    public MainController(MainService mainService) {
-        this.mainService = mainService;
-    }
 
     @GetMapping("hello")
     public String hello() {
