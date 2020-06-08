eeda0b9e8ec0be85d1cc3ecd979c12545a604c15
TheTOXIN <thetoksin@gmail.com>
2020-06-08 18:32:23 +0300

    a h a

--- src/main/java/com/memastick/backmem/notification/controller/NotifyWebController.java
+++ src/main/java/com/memastick/backmem/notification/controller/NotifyWebController.java
@@ -1,6 +1,7 @@
 package com.memastick.backmem.notification.controller;
 
 import com.memastick.backmem.notification.impl.NotifyWebService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,15 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/notify/web")
 public class NotifyWebController {
 
     private final NotifyWebService webService;
-
-    @Autowired
-    public NotifyWebController(NotifyWebService webService) {
-        this.webService = webService;
-    }
 
     @PutMapping("/register")
     public void registerWeb(@RequestBody String sessionId) {
