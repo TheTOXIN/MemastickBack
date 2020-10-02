9fed01979227bdf5d809ebbe376d01a040604375
TheTOXIN <thetoksin@gmail.com>
2020-10-02 21:56:26 +0300

    твоя мать кто

--- src/main/java/com/memastick/backmem/setting/controller/SettingFollowerController.java
+++ src/main/java/com/memastick/backmem/setting/controller/SettingFollowerController.java
@@ -2,22 +2,18 @@ package com.memastick.backmem.setting.controller;
 
 import com.memastick.backmem.memetick.api.MemetickPreviewAPI;
 import com.memastick.backmem.setting.service.SettingFollowerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("setting-followers")
 public class SettingFollowerController {
 
     private final SettingFollowerService settingFollowerService;
-
-    @Autowired
-    public SettingFollowerController(SettingFollowerService settingFollowerService) {
-        this.settingFollowerService = settingFollowerService;
-    }
 
     @PostMapping("/follow/{memetickId}")
     public void follow(@PathVariable("memetickId")UUID memetickId) {
